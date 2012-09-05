a060437c71d475de1ee6eac1987a94af1f24e394
Nathan Hwang <thenoviceoof@gmail.com>
2012-09-04 23:17:48 -0400

    Basic command line parsing

--- proxybfs.go
+++ proxybfs.go
@@ -10,14 +10,26 @@ import (
 
 // takes arguments, gives back a list of listeners, connectors, and
 // dict of other args
-func parseargs(args []string) ([]string, []string, map[string]string) {
-	listeners := []string{"world"}
-	connectors := []string{"hello"}
+func parseargs(args []string) (map[string]bool, map[string]bool, map[string]string) {
+	// using maps as a set
+	listeners := make(map[string]bool)
+	connectors := make(map[string]bool)
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		switch arg {
+		case "-l":
+			listeners[args[i+1]] = true
+		case "-c":
+			connectors[args[i+1]] = true
+		default:
+			fmt.Println("OH FUCK")
+		}
+	}
 	kwargs := make(map[string]string)
 	return listeners, connectors, kwargs
 }
 
 func main() {
-	parseargs(os.Args)
-	fmt.Println("Falling over my own feet here")
+	l,c,kw := parseargs(os.Args)
+	fmt.Println(l,c,kw)
 }
